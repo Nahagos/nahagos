b13f5179fcf78599966a36817ac5eba3b168c8ef
aa10 mechina <hanicha10aa@gmail.com>
2024-12-01 15:28:40 +0200

    added login page in main activity

--- Nahagos/app/src/main/java/com/nahagos/nahagos/MainActivity.java
+++ Nahagos/app/src/main/java/com/nahagos/nahagos/MainActivity.java
@@ -7,6 +7,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.widget.TextView;
+
+import com.nahagos.nahagos.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +18,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        TextView titleText = findViewById(R.id.welcomeText);
+        titleText.setText("Welcome to Nahagos!");
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
