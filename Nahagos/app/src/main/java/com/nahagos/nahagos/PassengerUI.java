5fdd85eb236adf2249de3999335139482591679b
goatOfLife <mechina.ori.18@gmail.com>
2024-12-02 20:20:09 +0200

    Added storing zones and stations

--- Nahagos/app/src/main/java/com/nahagos/nahagos/PassengerUI.java
+++ Nahagos/app/src/main/java/com/nahagos/nahagos/PassengerUI.java
@@ -12,10 +12,23 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.nahagos.nahagos.databinding.ActivityPassengerUiBinding;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 public class PassengerUI extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     private ActivityPassengerUiBinding binding;
+
+    private JSONArray _stops;
+    private JSONObject _zones;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +41,34 @@ public class PassengerUI extends FragmentActivity implements OnMapReadyCallback 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        try {
+            _stops = loadStops();
+            _zones = zonesFromStops(_stops);
+        } catch (JSONException | IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private JSONObject zonesFromStops(JSONArray stops) throws JSONException {
+        JSONObject obj = new JSONObject();
+        for (int i = 0; i < stops.length(); i++) {
+            if (!obj.has(stops.getJSONObject(i).getString("zone_id"))) {
+                obj.put(stops.getJSONObject(i).getString("zone_id"), new JSONArray());
+            }
+            obj.getJSONArray(stops.getJSONObject(i).getString("zone_id")).put(stops.getJSONObject(i).getString("stop_id"));
+        }
+        return obj;
+    }
+
+    private JSONArray loadStops() throws IOException, JSONException {
+        BufferedReader reader =new BufferedReader(new InputStreamReader(getAssets().open("bus_data/stops.json")));
+        StringBuilder output = new StringBuilder();
+        String tmp;
+        while ((tmp = reader.readLine()) != null) {
+            output.append(tmp);
+        }
+        return new JSONArray(output.toString());
     }
 
     /**
