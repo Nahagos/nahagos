89044ac021815ba592ec6fdbabc85416cfda4b8f
goatOfLife <mechina.ori.18@gmail.com>
2024-12-12 20:47:14 +0200

    Minor zoom param changes for better UX

--- Nahagos/app/src/main/java/com/nahagos/nahagos/SearchStopResult.java
+++ Nahagos/app/src/main/java/com/nahagos/nahagos/SearchStopResult.java
@@ -2,14 +2,14 @@ package com.nahagos.nahagos;
 
 import android.util.Pair;
 
-public class StopResult extends Pair<Integer, String> {
+public class SearchStopResult extends Pair<Integer, String> {
     /**
      * Constructor for a Pair.
      *
      * @param first  the first object in the Pair
      * @param second the second object in the pair
      */
-    public StopResult(Integer first, String second) {
+    public SearchStopResult(Integer first, String second) {
         super(first, second);
     }
 
