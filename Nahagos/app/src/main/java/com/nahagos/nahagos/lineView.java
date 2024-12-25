5cc7b2ef641dd9bdf641704e2c03831beb5a2314
goatOfLife <mechina.ori.18@gmail.com>
2024-12-25 21:31:13 +0200

    Changes for xml so ids wont intervene

--- Nahagos/app/src/main/java/com/nahagos/nahagos/lineView.java
+++ Nahagos/app/src/main/java/com/nahagos/nahagos/lineView.java
@@ -1,6 +1,7 @@
 package com.nahagos.nahagos;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +11,21 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class lineView extends AppCompatActivity {
 
+    private ListView stationsList;
+
+    private final int lineId = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_line_view);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.layout), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        stationsList = findViewById(R.id.stations_list);
     }
 }
