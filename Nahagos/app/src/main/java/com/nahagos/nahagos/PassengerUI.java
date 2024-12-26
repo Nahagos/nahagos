88c54247d17385fa5d2496f3a6bfc7380adbf0bb
goatOfLife <mechina.ori.18@gmail.com>
2024-12-26 20:51:48 +0200

    More "fixes", according to Lavi

--- Nahagos/app/src/main/java/com/nahagos/nahagos/PassengerUI.java
+++ Nahagos/app/src/main/java/com/nahagos/nahagos/PassengerUI.java
@@ -15,7 +15,6 @@ import com.google.android.gms.location.FusedLocationProviderClient;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SearchView;
@@ -49,16 +48,15 @@ public class PassengerUI extends FragmentActivity {
     private final float START_ZOOM = 15.5F;
     private final float STOP_ZOOM = 16.5F;
 
-    private GoogleMap _map;
-    private ActivityPassengerUiBinding binding;
+    private GoogleMap map;
     private ArrayAdapter<SearchStopResult> adapter;
     private ListView suggestionList;
 
-    private JSONArray _stops;
+    private JSONArray stops;
 
-    private ArrayList<SearchStopResult> _lastSearchRes = new ArrayList<>();
+    private ArrayList<SearchStopResult> lastSearchRes = new ArrayList<>();
 
-    private ArrayList<Marker> _stopMarkers = new ArrayList<>();
+    private ArrayList<Marker> stopMarkers = new ArrayList<>();
     public LatLng startingPoint = ISRAEL;
 
 
@@ -66,7 +64,7 @@ public class PassengerUI extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        binding = ActivityPassengerUiBinding.inflate(getLayoutInflater());
+        com.nahagos.nahagos.databinding.ActivityPassengerUiBinding binding = ActivityPassengerUiBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
@@ -78,22 +76,20 @@ public class PassengerUI extends FragmentActivity {
         suggestionList = findViewById(R.id.suggestions);
         SearchView search = findViewById(R.id.search);
 
-        adapter = new ArrayAdapter<>(getBaseContext(), R.layout.list_sample_element, R.id.textView, _lastSearchRes);
+        adapter = new ArrayAdapter<>(getBaseContext(), R.layout.list_sample_element, R.id.textView, lastSearchRes);
 
         suggestionList.setAdapter(adapter);
         try {
-            _stops = getStops();
+            stops = getStops();
         } catch (JSONException | IOException e) {
             throw new RuntimeException(e);
         }
 
-        search.setOnQueryTextFocusChangeListener(new View.OnFocusChangeListener() {
-            @Override
-            public void onFocusChange(View v, boolean hasFocus) {
+        search.setOnQueryTextFocusChangeListener((v, hasFocus) -> {
                 // Make search list visible or not based on whether the user is typing
-                suggestionList.setVisibility(hasFocus ? View.VISIBLE : View.INVISIBLE);
-            }
+            suggestionList.setVisibility(hasFocus ? View.VISIBLE : View.INVISIBLE);
         });
+
         search.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
@@ -103,39 +99,36 @@ public class PassengerUI extends FragmentActivity {
             @Override
             public boolean onQueryTextChange(String newText) {
                 // Search for q in the SQL, and update suggestion list as needed.
-                _lastSearchRes.clear();
+                lastSearchRes.clear();
                 try {
-                    _lastSearchRes = searchStations(newText);
+                    lastSearchRes = searchStations(newText);
                 } catch (JSONException e) {
                     throw new RuntimeException(e);
                 }
 
                 // This is the part of the code where we update the list of suggestions, based on the search results
-                if (_lastSearchRes.isEmpty()) {
-                    _lastSearchRes.add(new SearchStopResult(STOP_ID_NOT_FOUND, getString(R.string.stop_not_found)));
+                if (lastSearchRes.isEmpty()) {
+                    lastSearchRes.add(new SearchStopResult(STOP_ID_NOT_FOUND, getString(R.string.stop_not_found)));
                 }
 
                 adapter.clear();
-                adapter.addAll(_lastSearchRes);
+                adapter.addAll(lastSearchRes);
                 adapter.notifyDataSetChanged();
                 return false;
             }
         });
 
         // When a list item (i.e. search result) is clicked, move to its place.
-        suggestionList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                int stopId = _lastSearchRes.get((int) id).first;
-                if (stopId == STOP_ID_NOT_FOUND)
-                    return;
+        suggestionList.setOnItemClickListener((parent, view, position, id) -> {
+            int stopId = lastSearchRes.get((int) id).first;
+            if (stopId == STOP_ID_NOT_FOUND)
+                return;
 
-                try {
-                    JSONObject stop = _stops.getJSONObject(stopId);
-                    _map.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(stop.getDouble("stop_lat"), stop.getDouble("stop_lon")), STOP_ZOOM));
-                } catch (JSONException e) {
-                    throw new RuntimeException(e);
-                }
+            try {
+                JSONObject stop = stops.getJSONObject(stopId);
+                map.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(stop.getDouble("stop_lat"), stop.getDouble("stop_lon")), STOP_ZOOM));
+            } catch (JSONException e) {
+                throw new RuntimeException(e);
             }
         });
     }
@@ -145,9 +138,9 @@ public class PassengerUI extends FragmentActivity {
     */
     ArrayList<SearchStopResult> searchStations(String q) throws JSONException {
         ArrayList<SearchStopResult> out = new ArrayList<>();
-        for (int i = 0; i < _stops.length(); i++) {
-            if (_stops.getJSONObject(i).getString("stop_name").contains(q)) {
-                out.add(new SearchStopResult(i, _stops.getJSONObject(i).getString("stop_name")));
+        for (int i = 0; i < stops.length(); i++) {
+            if (stops.getJSONObject(i).getString("stop_name").contains(q)) {
+                out.add(new SearchStopResult(i, stops.getJSONObject(i).getString("stop_name")));
             }
         }
         return out;
@@ -168,39 +161,34 @@ public class PassengerUI extends FragmentActivity {
     }
 
     public void onMapReady(GoogleMap googleMap) {
-        _map = googleMap;
-        _map.setOnMarkerClickListener(this::onMarkerClick);
+        map = googleMap;
+        map.setOnMarkerClickListener(this::onMarkerClick);
 
         if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED )
         {
             ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 1);
-            return;
         }
-        _map.setMyLocationEnabled(true);
-        _map.moveCamera(CameraUpdateFactory.newLatLngZoom(startingPoint, START_ZOOM));
+        map.setMyLocationEnabled(true);
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(startingPoint, START_ZOOM));
 
-        FusedLocationProviderClient fusedLocationProviderClient = fusedLocationProviderClient =     LocationServices.getFusedLocationProviderClient(this);;
-        fusedLocationProviderClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
-            @Override
-            public void onComplete(@NonNull Task<Location> task) {
-                if (task.isSuccessful() && task.getResult() != null) {
-                    // Get the location
-                    Location location = task.getResult();
-                    double latitude = location.getLatitude();
-                    double longitude = location.getLongitude();
+        FusedLocationProviderClient fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);;
+        fusedLocationProviderClient.getLastLocation().addOnCompleteListener((task) -> {
+            if (task.isSuccessful() && task.getResult() != null) {
+                // Get the location
+                Location location = task.getResult();
+                double latitude = location.getLatitude();
+                double longitude = location.getLongitude();
 
-                    startingPoint = new LatLng(latitude, longitude);
-                    _map.moveCamera(CameraUpdateFactory.newLatLngZoom(startingPoint, START_ZOOM));
-                    Log.d("PassengerUI",
-                            "KAKIIIIIIIIIIIIIIIIII Latitude: " + latitude + ", Longitude: " + longitude);
-
-                }
+                startingPoint = new LatLng(latitude, longitude);
+                map.moveCamera(CameraUpdateFactory.newLatLngZoom(startingPoint, START_ZOOM));
+                Log.d("PassengerUI",
+                        "KAKIIIIIIIIIIIIIIIIII Latitude: " + latitude + ", Longitude: " + longitude);
 
             }
             });
 
 
-        _map.setOnCameraMoveListener(() -> {
+        map.setOnCameraMoveListener(() -> {
             /* if moved, you need to show the markers that are in the view.
              how the algorithm works:
              there is a list of stopmarkers, those are the markers that we show right now.
@@ -208,29 +196,29 @@ public class PassengerUI extends FragmentActivity {
              then, if the zoom is big enough - search for stops that are in range of the screen, add to the list, and show them.
              */
             // Search for markers that are shown in the map and not in range, and remove them.
-            LatLngBounds mapViewBounds = _map.getProjection().getVisibleRegion().latLngBounds;
-            for (int i = 0; i < _stopMarkers.size(); i++) {
-                LatLng markerPos = _stopMarkers.get(i).getPosition();
+            LatLngBounds mapViewBounds = map.getProjection().getVisibleRegion().latLngBounds;
+            for (int i = 0; i < stopMarkers.size(); i++) {
+                LatLng markerPos = stopMarkers.get(i).getPosition();
                 if (markerPos.latitude > mapViewBounds.northeast.latitude ||
                         markerPos.latitude < mapViewBounds.southwest.latitude ||
                         markerPos.longitude > mapViewBounds.northeast.longitude ||
                         markerPos.longitude < mapViewBounds.southwest.longitude) {
-                    _stopMarkers.get(i).remove();
-                    _stopMarkers.remove(i);
+                    stopMarkers.get(i).remove();
+                    stopMarkers.remove(i);
                 }
             }
             // if the zoom is big enough, search for stops that are in range, and show them on the map
-            if (_map.getCameraPosition().zoom >= ZOOM_SHOW_STOPS) {
-                for (int i = 0; i < _stops.length(); i++) {
+            if (map.getCameraPosition().zoom >= ZOOM_SHOW_STOPS) {
+                for (int i = 0; i < stops.length(); i++) {
                     try {
-                        LatLng stopLatLng = new LatLng(_stops.getJSONObject(i).getDouble("stop_lat"), _stops.getJSONObject(i).getDouble("stop_lon"));
+                        LatLng stopLatLng = new LatLng(stops.getJSONObject(i).getDouble("stop_lat"), stops.getJSONObject(i).getDouble("stop_lon"));
                         if (stopLatLng.latitude <= mapViewBounds.northeast.latitude &&
                                 stopLatLng.latitude >= mapViewBounds.southwest.latitude &&
                                 stopLatLng.longitude <= mapViewBounds.northeast.longitude &&
                                 stopLatLng.longitude >= mapViewBounds.southwest.longitude) {
-                            _stopMarkers.add(_map.addMarker(new MarkerOptions()
-                                    .position(new LatLng(_stops.getJSONObject(i).getDouble("stop_lat"), _stops.getJSONObject(i).getDouble("stop_lon")))
-                                    .title(_stops.getJSONObject(i).getString("stop_name") + " | " + _stops.getJSONObject(i).getString("stop_code"))));
+                            stopMarkers.add(map.addMarker(new MarkerOptions()
+                                    .position(new LatLng(stops.getJSONObject(i).getDouble("stop_lat"), stops.getJSONObject(i).getDouble("stop_lon")))
+                                    .title(stops.getJSONObject(i).getString("stop_name") + " | " + stops.getJSONObject(i).getString("stop_code"))));
                         }
                     } catch (JSONException e) {
                         throw new RuntimeException(e);
