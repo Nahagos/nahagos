189a69c3ae8487624e5340003cd7f5f6802a1129
Idan Mechina <idan.mechina@gmail.com>
2025-01-01 14:18:58 +0200

    Refactored class fields

--- Nahagos/app/src/main/java/com/nahagos/nahagos/Line.java
+++ Nahagos/app/src/main/java/com/nahagos/nahagos/Line.java
@@ -1,18 +1,18 @@
 package com.nahagos.nahagos;
 
 public class Line {
-    public String trip_id;       // Identifier for the trip
+    public String tripId;       // Identifier for the trip
     public String name;         // Name of the line
-    public int line_num;         // Line number
+    public int lineNum;         // Line number
     public String departure;    // Departure time
     public String operator;     // Operator of the line
     public boolean isNahagos;   // Whether it is Nahagos line or not
 
     // Constructor
-    public Line(String trip_id, String name, int line_num, String departure, String operator, boolean isNahagos) {
-        this.trip_id = trip_id;
+    public Line(String tripId, String name, int lineNum, String departure, String operator, boolean isNahagos) {
+        this.tripId = tripId;
         this.name = name;
-        this.line_num = line_num;
+        this.lineNum = lineNum;
         this.departure = departure;
         this.operator = operator;
         this.isNahagos = isNahagos;
@@ -23,9 +23,9 @@ public class Line {
     @Override
     public String toString() {
         return "Line{" +
-                "trip_id='" + trip_id + '\'' +
+                "tripId='" + tripId + '\'' +
                 ", name='" + name + '\'' +
-                ", line_num=" + line_num +
+                ", lineNum=" + lineNum +
                 ", departure='" + departure + '\'' +
                 ", operator='" + operator + '\'' +
                 ", isNahagos=" + isNahagos +
