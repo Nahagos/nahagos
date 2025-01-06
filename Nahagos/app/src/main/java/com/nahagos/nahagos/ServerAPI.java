70174b7cf05763531ff5fd02b9212c432e905ca0
Idan Mechina <idan.mechina@gmail.com>
2025-01-06 20:47:59 +0200

    changed post and get request to return object of class

--- Nahagos/app/src/main/java/com/nahagos/nahagos/ServerAPI.java
+++ Nahagos/app/src/main/java/com/nahagos/nahagos/ServerAPI.java
@@ -28,154 +28,83 @@ public class ServerAPI {
         ROOT_URL = "http://" + context.getString(R.string.server_ip) + ":8000";
     }
 
-    public void passengerLogin(String username, String password)
+    public boolean passengerLogin(String username, String password)
     {
         String jsonBody = "{\"username\": \"" + username + "\", \"password\":\"" + password + "\"}";
-        Networks.httpPostReq(ROOT_URL + PASSENGER_LOGIN_URL, jsonBody);
+        Gson response = Networks.httpPostReq(ROOT_URL + PASSENGER_LOGIN_URL, jsonBody, Gson.class);
+        Log.d(TAG, "login response: " + response);
+        return response != null;
+
     }
 
     // Register a new passenger - client method
     public boolean passenger_signup(String username, String password)
     {
          String jsonBody = "{\"username\": \"" + username + "\", \"password\":\"" + password + "\"}";
-        String response = Networks.httpPostReq(ROOT_URL + REGISTER_URL, jsonBody);
-        return !response.startsWith("Error");
+         Gson response = Networks.httpPostReq(ROOT_URL + REGISTER_URL, jsonBody, Gson.class);
+         return response != null;
     }
 
-    public void driverLogin(String username, String password, String id)
+    public boolean driverLogin(String username, String password, String id)
     {
         String jsonBody = "{\"username\": \"" + username + "\", \"password\":\"" + password + "\","+"\"id\":\""+id+"\"}";
-        Networks.httpPostReq(ROOT_URL + DRIVER_LOGIN_URL, jsonBody);
+        Gson response = Networks.httpPostReq(ROOT_URL + DRIVER_LOGIN_URL, jsonBody, Gson.class);
+        Log.d(TAG, "login response: " + response);
+        return response != null;
+
     }
 
     //get lines from a given station - client method
     public Line[] get_lines_by_station(int stopId) {
         String url = ROOT_URL + GET_LINES_BY_STATION_URL + stopId;
-        String response = Networks.httpGetReq(url);
+        Line[] response = Networks.httpGetReq(url, Line[].class);
 
-        if (response.startsWith("Error:")) {
-            Log.e(TAG, response); // Log the error
-            return null; // Handle the error as needed (e.g., return null or notify the user)
-        }
-
-        try {
-            Gson gson = new Gson();
-            return gson.fromJson(response, Line[].class); // Parse the JSON into Line[] array
-        } catch (Exception e) {
-            Log.e(TAG, "Error parsing response to Line[]", e);
-            return null; // Handle parsing errors as needed
-        }
+        return response;
     }
 
     //passenger method - wait for a passenger at a given station
-    public boolean wait_for_me(int trip_id, int stop_id)
+    public boolean wait_for_me(String trip_id, int stop_id)
     {
-        String jsonBody = "{\"trip_id\": " + trip_id + ", \"stop_id\":" + stop_id + "}";
-        String response = Networks.httpPostReq(ROOT_URL + WAIT_FOR_ME_URL, jsonBody);
-        return !response.startsWith("Error");
+        String jsonBody = "{\"trip_id\": \"" + trip_id + "\", \"stop_id\":" + stop_id + "}";
+        Gson response = Networks.httpPostReq(ROOT_URL + WAIT_FOR_ME_URL, jsonBody, Gson.class);
+        return response != null;
     }
 
     //register for a line - driver method
-    public boolean register_for_line(int trip_id)
+    public boolean register_for_line(String trip_id)
     {
-        String jsonBody = "{\"trip_id\": " + trip_id + "}";
-        String response = Networks.httpPostReq(ROOT_URL + REGISTER_FOR_LINE_URL, jsonBody);
-        return !response.startsWith("Error");
+        String jsonBody = "{\"trip_id\": \"" + trip_id + "\"}";
+        Gson response = Networks.httpPostReq(ROOT_URL + REGISTER_FOR_LINE_URL, jsonBody, Gson.class);
+        return response != null;
     }
 
     //get driver schedule - driver method
     public Gson get_driver_schedule()
     {
-        String response = Networks.httpGetReq(ROOT_URL + GET_DRIVER_SCHEDULE_URL);
-        if (response.startsWith("Error"))
-        {
-            Log.e(TAG, response); // Log the error
-            return null;
-        }
-        else
-        {
-            try
-                {
-                Gson gson = new Gson();
-                return gson.fromJson(response, Gson.class);
-            }
-            catch (Exception e)
-            {
-                Log.e(TAG, "Error parsing response to Gson", e);
-                return null;
-            }
-        }
-
+        Gson response = Networks.httpGetReq(ROOT_URL + GET_DRIVER_SCHEDULE_URL, Gson.class);
+        return response;
     }
 
     //get stopping stations - driver method
     public int[] get_stopping_stations()
     {
         String url = ROOT_URL + GET_STOPPING_STATIONS_URL;
-        String response = Networks.httpGetReq(url);
-        if (response.startsWith("Error:")) {
-            Log.e(TAG, response); // Log the error
-            return null; // Handle the error as needed (e.g., return null or notify the user)
-        }
-
-        try {
-            Gson gson = new Gson();
-            return gson.fromJson(response, int[].class); // Parse the JSON into int[] array
-        } catch (Exception e) {
-            Log.e(TAG, "Error parsing response to int[]", e);
-            return null; // Handle parsing errors as needed
-        }
+        int[] response = Networks.httpGetReq(url, int[].class);
+        return response;
     }
 
     // get line stops - client method
     public StopTime[] get_stops_by_line(int trip_id)
     {
         String url = ROOT_URL + GET_STOPS_BY_LINE_URL + trip_id;
-        String response = Networks.httpGetReq(url);
-        if (response.startsWith("Error:")) {
-            Log.e(TAG, response); // Log the error
-            return null; // Handle the error as needed (e.g., return null or notify the user)
-        }
-
-        else
-        {
-            try
-            {
-                Gson gson = new Gson();
-                return gson.fromJson(response, StopTime[].class);
-            }
-            catch (Exception e)
-            {
-                Log.e(TAG, "Error parsing response to Gson", e);
-                return null;
-            }
-        }
+        StopTime[] response = Networks.httpGetReq(url, StopTime[].class);
+        return  response;
     }
 
     // get shape of a given line - driver method
     public Point[] get_line_shape(int trip_id)
     {
-        String response = Networks.httpGetReq(ROOT_URL + GET_LINE_SHAPE + trip_id);
-        if (response.startsWith("Error"))
-        {
-            Log.e(TAG, response); // Log the error
-            return null;
-        }
-        else
-        {
-            try {
-                Gson gson = new Gson();
-                JsonObject jsonResponse = JsonParser.parseString(response).getAsJsonObject();
-                if (!jsonResponse.has("Shape")) {
-                    Log.e(TAG, "Missing 'Shape' field in response");
-                    return null;
-                }
-
-                return gson.fromJson(jsonResponse.getAsJsonArray("Shape"), Point[].class);
-            } catch (JsonSyntaxException e) {
-                Log.e(TAG, "Error parsing 'Shape' field", e);
-                return null;
-            }
-        }
+        Point[] response = Networks.httpGetReq(ROOT_URL + GET_LINE_SHAPE + trip_id, Point[].class);
+        return response;
     }
 }
