f7fdc8066b7926351709336662d7891c5288d40a
number10mechina <hanicha10aa@gmail.com>
2025-01-06 21:33:23 +0200

    Application/activity merging (#17)
    * added prototype for app and basics folders
    
    * added prototype for app and basics folders
    
    * move app to main directory
    
    * added server folder
    
    * Revert "move app to main directory"
    
    This reverts commit 43f39330f4cb1116818f6546a8536a62310b3f32.
    
    * Enforce LF line endings
    
    * added app branch
    
    * Reboot the project to a blank one
    
    * fixes for reboot
    
    * GitHub Actions - build APKs (#1)
    
    * Create generate-apk-aab-debug-release.yml
    
    * run for prs
    
    * remove ref to a dead activity
    
    * actually run for all prs
    
    * Local DB Manager (#3)
    
    * Initial version for DBManager.java
    
    * initial stops db room setup and an app wide reformat
    
    * refactor and optimize room imports, refactor Tables class
    
    * Login page (#6)
    
    * added login page in main activity
    
    * added 2 new logos for the app
    
    * fixed the netwroks java class interface
    
    * added diffrenet interface for the networks utils for the login part, used native
    
    * commuinication in server is working, adding a new interface for POST requests
    
    * added checkbox in login page to indicate if the user is a driver
    
    * fixed commuinication with server, added cookies to packets
    
    * Added a serverAPI interface between driver&passenger
    
    * finished login both for driver and passanger successfully with the server
    
    * cleared mainActivity
    
    * fixed login ui bugs
    
    * added jsonObject to the project
    
    * reviewing testing with server
    
    * final changes for 1.0.0
    
    * fixed pr comments; removed unneccessry logs & imports
    
    ---------
    
    Co-authored-by: root <root@DESKTOP-5QV5963>
    Co-authored-by: goatOfLife <mechina.ori.18@gmail.com>
    
    * Passenger (#7)
    
    * added app branch
    
    * Reboot the project to a blank one
    
    * fixes for reboot
    
    * Made an activity with a map and search ui
    
    * Added pins based on zones (Work in progress)
    
    * Made an activity with a map and search ui
    
    * Added pins based on zones (Work in progress)
    
    * Added storing zones and stations
    
    * show stops based on map view (in progress)
    
    * Map shows stations based on camera position and zoom
    
    * Map removes stations previously shown that are out of screen
    
    * Start of getting user location
    
    * Start with searching stations
    
    * Search map feature
    
    * Minor zoom param changes for better UX
    
    * Comments and minor adjustments to Search
    
    * Added GPS coordiants extractor
    
    * Add stop names for shown stops
    
    Also foundation for showing lines arriving for a specific station
    
    * gps changes
    
    * added dependenicies packeges for current gps location:
    
    * tested for current gps location
    
    * added israel point view
    
    * Ori's CR part fixes
    
    * fixed gps starting location and permmisions
    
    * optimize list updates by reusing the adapter
    
    * More of Ori's fixes, CR
    
    * Merged adapter changes
    
    * optimize list updates by reusing the adapter
    
    * More little fixes before merge
    
    * More "fixes", according to Lavi
    
    * Fix more CR after merging from app to passenger
    
    * Fixing LastSearchRes to be final
    
    * Remove unnecessary comment
    
    ---------
    
    Co-authored-by: goatOfLife <mechina.ori.18@gmail.co>
    Co-authored-by: goatOfLife <mechina.ori.18@gmail.com>
    Co-authored-by: olivecylinder <olivecylinder@gmail.com>
    
    * relocate nahagos to Nahagos (#12)
    
    * add app, dev, and main branches (#13)
    
    * touchups, fixing build error (#14)
    
    * merging all of the activites togther
    
    * connected the activities to one single app
    
    ---------
    
    Co-authored-by: olivecylinder <olivecylinder@gmail.com>
    Co-authored-by: goatOfLife <mechina.ori.18@gmail.co>
    Co-authored-by: root <root@DESKTOP-5QV5963>
    Co-authored-by: goatOfLife <mechina.ori.18@gmail.com>

---
+++ Nahagos/app/src/main/java/com/nahagos/nahagos/Networks.java
@@ -0,0 +1,96 @@
+package com.nahagos.nahagos;
+
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class Networks {
+    private static final String TAG = "HTTP";
+
+
+    // Helper method to set up a connection
+    private static HttpURLConnection setupConnection(String urlString, String method) throws Exception {
+        URL url = new URL(urlString);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod(method);
+        connection.setConnectTimeout(5000); // 5sec timeout
+        connection.setReadTimeout(5000);
+
+        if ("POST".equalsIgnoreCase(method)) {
+            connection.setDoOutput(true); // Enable output for POST
+            connection.setRequestProperty("Content-Type", "application/json");
+        }
+
+        return connection;
+    }
+
+    // Helper method to read response
+    private static String readResponse(HttpURLConnection connection) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        StringBuilder response = new StringBuilder();
+        String line;
+
+        while ((line = reader.readLine()) != null) {
+            response.append(line);
+        }
+
+        reader.close();
+        return response.toString();
+    }
+
+    // Method for HTTP GET request
+    public static void httpGetReq(String urlString) {
+        HttpURLConnection connection = null;
+
+        try {
+            connection = setupConnection(urlString, "GET");
+            int responseCode = connection.getResponseCode();
+            Log.d(TAG, "Response Code: " + responseCode);
+
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                String response = readResponse(connection);
+
+                Log.d(TAG, "Response: " + response);
+            }
+        } catch (Exception e) {
+            Log.e(TAG, "Error in HTTP GET request", e);
+        } finally {
+            if (connection != null) {
+                connection.disconnect();
+            }
+        }
+    }
+
+    public static int httpPostReq(String urlString, String postData) {
+        HttpURLConnection connection = null;
+        int responseCode = -1;
+        try {
+            connection = setupConnection(urlString, "POST");
+
+            // Write data to the output stream
+            try (OutputStreamWriter writer = new OutputStreamWriter(connection.getOutputStream(), "UTF-8")) {
+                writer.write(postData);
+                writer.flush();
+            }
+
+            responseCode = connection.getResponseCode();
+            Log.d(TAG, "Response Code: " + responseCode);
+
+            if (responseCode == HttpURLConnection.HTTP_OK || responseCode == HttpURLConnection.HTTP_CREATED) {
+                String response = readResponse(connection);
+                Log.d(TAG, "Response: " + response);
+            }
+        } catch (Exception e) {
+            Log.e(TAG, "Error in HTTP POST request", e);
+        } finally {
+            if (connection != null) {
+                connection.disconnect();
+            }
+            return responseCode;
+        }
+    }
+}
---
+++ Nahagos/app/src/main/java/com/nahagos/nahagos/ServerAPI.java
@@ -0,0 +1,25 @@
+package com.nahagos.nahagos;
+import android.content.Context;
+import com.nahagos.nahagos.Networks;
+
+public class ServerAPI {
+    private String PASSENGER_LOGIN_URL = "/passenger/login/";
+    private String DRIVER_LOGIN_URL = "/driver/login/";
+    private String ROOT_URL;
+
+    // Constructor accepting Context
+    public ServerAPI(Context context) {
+        ROOT_URL = "http://" + context.getString(R.string.server_ip) + ":8000";
+    }
+
+    public boolean passengerLogin(String username, String password)
+    {
+        String jsonBody = "{\"username\": \"" + username + "\", \"password\":\"" + password + "\"}";
+        return Networks.httpPostReq(ROOT_URL + PASSENGER_LOGIN_URL, jsonBody) == 200;
+    }
+    public boolean driverLogin(String username, String password, String id)
+    {
+        String jsonBody = "{\"username\": \"" + username + "\", \"password\":\"" + password + "\","+"\"id\":\""+id+"\"}";
+        return Networks.httpPostReq(ROOT_URL + DRIVER_LOGIN_URL, jsonBody) == 200;
+    }
+}
