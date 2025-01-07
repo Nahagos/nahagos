53ec36106e7e0cdc44baedeb542fe6b3dbe503c1
Idan Mechina <idan.mechina@gmail.com>
2025-01-07 09:51:39 +0200

    Enumerated endpoint access

--- Nahagos/app/src/main/java/com/nahagos/nahagos/ServerAPI.java
+++ Nahagos/app/src/main/java/com/nahagos/nahagos/ServerAPI.java
@@ -11,16 +11,29 @@ import com.nahagos.nahagos.datatypes.StopTime;
 
 
 public class ServerAPI {
-    private final String PASSENGER_LOGIN_URL = "/passenger/login/";
-    private final String DRIVER_LOGIN_URL = "/driver/login/";
-    private final String REGISTER_URL = "/passenger/register/";
-    private final String GET_LINES_BY_STATION_URL = "/lines-by-station/";
-    private final String WAIT_FOR_ME_URL = "/passenger/wait-for/";
-    private final String REGISTER_FOR_LINE_URL = "/driver/drive/register/";
-    private final String GET_DRIVER_SCHEDULE_URL = "/driver/schedule/";
-    private final String GET_STOPPING_STATIONS_URL = "/driver/where-to-stop/";
-    private final String GET_STOPS_BY_LINE_URL = "/stops-by-line/";
-    private final String GET_LINE_SHAPE = "/line-shape/";
+    public enum Endpoint {
+        PASSENGER_LOGIN("/passenger/login/"),
+        DRIVER_LOGIN("/driver/login/"),
+        REGISTER("/passenger/register/"),
+        GET_LINES_BY_STATION("/lines-by-station/"),
+        WAIT_FOR_ME("/passenger/wait-for/"),
+        REGISTER_FOR_LINE("/driver/drive/register/"),
+        GET_DRIVER_SCHEDULE("/driver/schedule/"),
+        GET_STOPPING_STATIONS("/driver/where-to-stop/"),
+        GET_STOPS_BY_LINE("/stops-by-line/"),
+        GET_LINE_SHAPE("/line-shape/");
+
+        private final String url;
+
+        Endpoint(String url) {
+            this.url = url;
+        }
+
+        public String getUrl() {
+            return url;
+        }
+    }
+
     private final String ROOT_URL;
 
     // Constructor accepting Context
@@ -31,7 +44,7 @@ public class ServerAPI {
     public boolean passengerLogin(String username, String password)
     {
         String jsonBody = "{\"username\": \"" + username + "\", \"password\":\"" + password + "\"}";
-        Gson response = Networks.httpPostReq(ROOT_URL + PASSENGER_LOGIN_URL, jsonBody, Gson.class);
+        Gson response = Networks.httpPostReq(ROOT_URL + Endpoint.PASSENGER_LOGIN.getUrl(), jsonBody, Gson.class);
         Log.d(TAG, "login response: " + response);
         return response != null;
 
@@ -41,14 +54,14 @@ public class ServerAPI {
     public boolean passenger_signup(String username, String password)
     {
          String jsonBody = "{\"username\": \"" + username + "\", \"password\":\"" + password + "\"}";
-         Gson response = Networks.httpPostReq(ROOT_URL + REGISTER_URL, jsonBody, Gson.class);
+         Gson response = Networks.httpPostReq(ROOT_URL + Endpoint.REGISTER.getUrl(), jsonBody, Gson.class);
          return response != null;
     }
 
     public boolean driverLogin(String username, String password, String id)
     {
         String jsonBody = "{\"username\": \"" + username + "\", \"password\":\"" + password + "\","+"\"id\":\""+id+"\"}";
-        Gson response = Networks.httpPostReq(ROOT_URL + DRIVER_LOGIN_URL, jsonBody, Gson.class);
+        Gson response = Networks.httpPostReq(ROOT_URL + Endpoint.DRIVER_LOGIN.getUrl(), jsonBody, Gson.class);
         Log.d(TAG, "login response: " + response);
         return response != null;
 
@@ -56,7 +69,7 @@ public class ServerAPI {
 
     //get lines from a given station - client method
     public Line[] get_lines_by_station(int stopId) {
-        String url = ROOT_URL + GET_LINES_BY_STATION_URL + stopId;
+        String url = ROOT_URL + Endpoint.GET_LINES_BY_STATION.getUrl() + stopId;
         Line[] response = Networks.httpGetReq(url, Line[].class);
         if (response == null)
             return new Line[0];
@@ -67,7 +80,7 @@ public class ServerAPI {
     public boolean wait_for_me(String trip_id, int stop_id)
     {
         String jsonBody = "{\"trip_id\": \"" + trip_id + "\", \"stop_id\":" + stop_id + "}";
-        Gson response = Networks.httpPostReq(ROOT_URL + WAIT_FOR_ME_URL, jsonBody, Gson.class);
+        Gson response = Networks.httpPostReq(ROOT_URL + Endpoint.WAIT_FOR_ME.getUrl(), jsonBody, Gson.class);
         return response != null;
     }
 
@@ -75,21 +88,21 @@ public class ServerAPI {
     public boolean register_for_line(String trip_id)
     {
         String jsonBody = "{\"trip_id\": \"" + trip_id + "\"}";
-        Gson response = Networks.httpPostReq(ROOT_URL + REGISTER_FOR_LINE_URL, jsonBody, Gson.class);
+        Gson response = Networks.httpPostReq(ROOT_URL + Endpoint.REGISTER_FOR_LINE.getUrl(), jsonBody, Gson.class);
         return response != null;
     }
 
     //get driver schedule - driver method
     public Gson get_driver_schedule()
     {
-        Gson response = Networks.httpGetReq(ROOT_URL + GET_DRIVER_SCHEDULE_URL, Gson.class);
+        Gson response = Networks.httpGetReq(ROOT_URL + Endpoint.GET_DRIVER_SCHEDULE.getUrl(), Gson.class);
         return response;
     }
 
     //get stopping stations - driver method
     public int[] get_stopping_stations()
     {
-        String url = ROOT_URL + GET_STOPPING_STATIONS_URL;
+        String url = ROOT_URL + Endpoint.GET_STOPPING_STATIONS.getUrl();
         int[] response = Networks.httpGetReq(url, int[].class);
         if (response == null)
             return new int[0];
@@ -99,7 +112,7 @@ public class ServerAPI {
     // get line stops - client method
     public StopTime[] get_stops_by_line(String trip_id)
     {
-        String url = ROOT_URL + GET_STOPS_BY_LINE_URL + trip_id;
+        String url = ROOT_URL + Endpoint.GET_STOPS_BY_LINE.getUrl() + trip_id;
         StopTime[] response = Networks.httpGetReq(url, StopTime[].class);
         if (response == null)
             return new StopTime[0];
@@ -109,7 +122,7 @@ public class ServerAPI {
     // get shape of a given line - driver method
     public LatLng[] get_line_shape(String trip_id)
     {
-        LatLng[] response = Networks.httpGetReq(ROOT_URL + GET_LINE_SHAPE + trip_id, LatLng[].class);
+        LatLng[] response = Networks.httpGetReq(ROOT_URL + Endpoint.GET_LINE_SHAPE.getUrl() + trip_id, LatLng[].class);
         if (response == null)
             return new LatLng[0];
         return response;
