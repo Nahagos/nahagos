db5059c1d617c0dec408f94029873278343561e5
Idan Mechina <idan.mechina@gmail.com>
2025-01-23 01:48:38 +0200

    removed unused imports

--- Nahagos/app/src/main/java/com/nahagos/nahagos/activities/Login.java
+++ Nahagos/app/src/main/java/com/nahagos/nahagos/activities/Login.java
@@ -1,7 +1,6 @@
 package com.nahagos.nahagos.activities;
 
 import android.content.Intent;
-import android.graphics.Paint;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
